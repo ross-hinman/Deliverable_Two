e2c89e9e22f0007fccd98fedc7ca449d14063f5b
ross-hinman <hinman.ross@gmail.com>
2018-07-03 18:54:10 -0400

    First commit for deliverable two

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deliverable_Two
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime myValue = DateTime.Now;
+
+            DateTime myBirthday = DateTime.Parse("7/29/1993");
+            TimeSpan myAge = DateTime.Now.Subtract(myBirthday);
+
+            Console.WriteLine("Difference in time between now and my birthday");
+            Console.WriteLine("");
+            Console.WriteLine(myAge.TotalDays);
+            Console.WriteLine("Days");
+            Console.WriteLine(myAge.TotalHours);
+            Console.WriteLine("Hours");
+            Console.WriteLine(myAge.TotalMinutes);
+            Console.WriteLine("Minutes");
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
